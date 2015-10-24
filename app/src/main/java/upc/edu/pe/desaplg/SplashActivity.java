fbb9c4af73d5604946dc2e2a443c53a410c1d1a1
dccruzt <dccruzt@gmail.com>
2015-10-23 21:09:01 -0500

    Creacion de estructura principal del proyecto

---
+++ app/src/main/java/upc/edu/pe/desaplg/SplashActivity.java
@@ -0,0 +1,16 @@
+package upc.edu.pe.desaplg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Daniela on 23/10/2015.
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView();
+    }
+}
