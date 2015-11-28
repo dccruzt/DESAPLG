e65003123a98e4525f36bcfd95e7c794eb611ec1
dccruzt <dccruzt@gmail.com>
2015-11-28 16:45:44 -0500

    Daniela: Últimos cambios de estilo

--- app/src/main/java/upc/edu/pe/desaplg/InicioActivity.java
+++ app/src/main/java/upc/edu/pe/desaplg/InicioActivity.java
@@ -3,6 +3,11 @@ package upc.edu.pe.desaplg;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import upc.edu.pe.desaplg.helpers.FontHelper;
 
 /**
  * Created by Daniela on 23/10/2015.
@@ -15,5 +20,14 @@ public class InicioActivity extends Activity{
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.layout_inicio);
+
+        TextView lblBienvenida = (TextView)findViewById(R.id.lblBienvenida);
+        FontHelper.setFont(this.getApplicationContext(), FontHelper.DOSIS_EXTRABOLD, lblBienvenida);
+
+        EditText txtConectar = (EditText)findViewById(R.id.txtConectar);
+        FontHelper.setFont(this.getApplicationContext(), FontHelper.DOSIS_EXTRABOLD, txtConectar);
+
+        Button btnConectar = (Button)findViewById(R.id.btnConectar);
+        FontHelper.setFont(this.getApplicationContext(), FontHelper.DOSIS_EXTRABOLD, btnConectar);
     }
 }
