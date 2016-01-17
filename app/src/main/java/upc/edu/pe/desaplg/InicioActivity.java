ddeead01554c667f039677bd51d4f85fef17a6e5
dccruzt <dccruzt@gmail.com>
2016-01-17 15:45:55 -0500

    Daniela: Se agregaron las imágenes de los avatares

--- app/src/main/java/upc/edu/pe/desaplg/InicioActivity.java
+++ app/src/main/java/upc/edu/pe/desaplg/InicioActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
@@ -44,32 +45,42 @@ public class InicioActivity extends Activity{
 
     public void conectar(View v){
 
-        ConnectionHelper.webAppSession.sendMessage(new JSONObject() {
-            {
-                try{
+        EditText txtConectar = (EditText)findViewById(R.id.txtConectar);
+
+        if(txtConectar.getText().toString().trim().length() == 0) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Debe ingresar un nombre para conectarse", 2);
+            toast.show();
+        }
+        else {
+            ConnectionHelper.webAppSession.sendMessage(new JSONObject() {
+                {
+                    try {
+                        EditText nombreJugador = (EditText) findViewById(R.id.txtConectar);
+                        put("accion", "enviarNombres");
+                        put("jugador", nombreJugador.getText());
+                        put("avatar", "Avatar_Hombre_1");
+                    } catch (JSONException ex) {
+                    }
+                }
+            }, new ResponseListener<Object>() {
+                @Override
+                public void onSuccess(Object o) {
                     EditText nombreJugador = (EditText) findViewById(R.id.txtConectar);
-                    put("accion", "enviarNombres");
-                    put("jugador",  nombreJugador.getText());
-                    put("avatar", "Avatar_Hombre_1");
+                    nombreJugador.setText("");
+                    Context context = getApplicationContext();
+                    Toast toast = Toast.makeText(context, "Conectado", 3);
+                    toast.show();
                 }
-                catch (JSONException ex){
+
+                @Override
+                public void onError(ServiceCommandError serviceCommandError) {
                 }
-            }
-        }, new ResponseListener<Object>() {
-            @Override
-            public void onSuccess(Object o) {
-                EditText nombreJugador = (EditText) findViewById(R.id.txtConectar);
-                nombreJugador.setText("");
-                Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, "Conectado", 3);
-                toast.show();
-            }
+            });
+        }
+    }
 
-            @Override
-            public void onError(ServiceCommandError serviceCommandError) {
+    public void cambiarImagen(View image) {
 
-            }
-        });
-
+        image.setSelected(!image.isSelected());
     }
 }
